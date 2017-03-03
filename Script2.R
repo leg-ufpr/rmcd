612a570f8c89dd03469b43486a096a8bcf2f00a4
wbonat <wbonat@gmail.com>
2017-03-03 17:59:41 -0300

    Chapter 2 - Finished

--- Script2.R
+++ Script2.R
@@ -2,15 +2,57 @@
 # Author: Wagner Hugo Bonat LEG/UFPR -----------------------------------
 # Date: 27/02/2017 -----------------------------------------------------
 
-# Probability mass function --------------------------------------------
-dptw <- function(y, mu, phi, power, control_sample) {
-    pts <- control_sample$pts
-    norma <- control_sample$norma
+# Integrand ------------------------------------------------------------
+integrand <- function(x, y, mu, phi, power) {
+    int = dpois(y, lambda = x)*dtweedie(x, mu = mu, 
+                                        phi = phi, power = power)
+    return(int)
+}
+
+# Numerical integration using Gauss-Laguerre method --------------------
+gauss_laguerre <- function(integrand, n_pts, y, mu, phi, power) {
+    pts <- gauss.quad(n_pts, kind="laguerre")
+    integral <- sum(pts$weights*integrand(pts$nodes, y = y, mu = mu, 
+                                          phi = phi, power = power)/
+                        exp(-pts$nodes))
+    return(integral)
+}
+
+gauss_laguerre_vec <- Vectorize(FUN = gauss_laguerre, vectorize.args = "y")
+
+# Numerical integration using Monte Carlo method -----------------------
+monte_carlo <- function(integrand, n_pts, y, mu, phi, power) {
+    pts <- rtweedie(n_pts, mu = mu, phi = phi, power = power)
+    norma <- dtweedie(pts, mu = mu, phi = phi, power = power)
     integral <- mean(integrand(pts, y = y, mu = mu, phi = phi, 
                                power = power)/norma)
     return(integral)
 }
-dptw <- Vectorize(dptw, vectorize.args = "y")
+
+# Probability mass function Poisson-Tweedie ----------------------------
+dptweedie_aux <- function(y, mu, phi, power, n_pts, method) {
+    if(method == "laguerre" | y > 0) {
+        pmf <- gauss_laguerre(integrand = integrand, n_pts = n_pts, 
+                              y = y, mu = mu, phi = phi, power = power)
+    }
+    if(method == "laguerre" & y == 0) {
+        v.y <- round(10*sqrt(mu + phi*mu^power),0)
+        if(v.y > 1000) {v.y <- 1000}
+        #print(v.y)
+        y.grid <- 0:v.y
+        temp <- gauss_laguerre_vec(integrand = integrand, n_pts = n_pts,
+                                   y = y.grid, mu = mu, phi = phi,
+                                   power = power)
+        pmf <- 1-sum(temp)+temp[1]
+    }
+    if(method == "MC") {
+        pmf <- monte_carlo(integrand = integrand, n_pts = n_pts, 
+                           y = y, mu = mu, phi = phi, power = power)
+    }
+    return(pmf)
+}
+# Vectorize version
+dptw <- Vectorize(FUN = dptweedie_aux, vectorize.args = c("y","mu"))
 
 # Simulating from Poisson-Tweedie models -------------------------------
 rptweedie <- function(n, mu, phi, power) {
@@ -25,26 +67,50 @@ rptweedie <- function(n, mu, phi, power) {
 }
 
 # Plot function --------------------------------------------------------
-plot_ptw <- function(mu, phi, power, title, n_sample = 100000, 
-                     grid_y = 0:100, mc_sample = 5000) {
-    control_sample <- list()
-    if(power <= 2) {
-    control_sample$pts <- rtweedie(n = mc_sample, mu = mu, 
-                                   phi = phi, power = power)
-    control_sample$norma <- dtweedie(control_sample$pts, mu = mu, 
-                                     phi = phi, power = power)
-    }
-    if(power > 2) {
-        control_sample$pts <- rinvgauss(n = mc_sample, mean = mu, 
-                                       dispersion = phi)
-        control_sample$norma <- dinvgauss(control_sample$pts, 
-                                          mean = mu, dispersion = phi)
-    }
+plot_ptw <- function(mu, phi, power, title, n_sample = 100000, n_pts = 25, method = "laguerre") {
+    require(statmod)
+    grid_y <- 0:100
     obs <- rptweedie(n = n_sample, mu = mu, phi = phi, power = power)
-    dens <- dptw(y = grid_y, mu = mu, phi = phi, power = power, 
-                 control_sample = control_sample)
+    dens <- dptw(y = grid_y, mu = mu, phi = phi, power = power, n_pts = n_pts, method = method)
     plot(table(obs)/n_sample, ylab = "Mass function", xlab = "y", col = "gray", 
          main = title)
     nn <- length(grid_y)-1
     lines(c(0:nn), dens, lty = 2, lwd = 2, col = "black", type = "l")
 }
+
+# Expectation and variance ---------------------------------------------
+moments_ptw <- function(mu, phi, power) {
+    expec <- mu
+    variance <- mu + phi*(mu^power)
+    return(c("Expectation" = expec, "Variance" = variance))
+}
+moments_ptw <- Vectorize(moments_ptw, c("mu"))
+
+# Dispersion index ---------------------------------------------------
+disp_index_ptw <- function(mu, phi, power) {
+    TEMP <- moments_ptw(mu = mu, phi = phi, power = power)
+    DI <- TEMP[2]/TEMP[1]
+    return(DI)
+}
+disp_index_ptw <- Vectorize(disp_index_ptw, c("mu"))
+
+# Zero-inflation index -----------------------------------------------
+zero_inflation_ptw <- function(mu, phi, power) {
+    PX0 <- dptw(y = 0, mu = mu, phi = phi, power = power, n_pts = 30, 
+                method = "laguerre")
+    mu <- moments_ptw(mu = mu, phi = phi, power = power)[1]
+    ZI <- 1 + log(PX0)/mu
+    return(ZI)
+}
+zero_inflation_ptw <- Vectorize(zero_inflation_ptw, c("mu"))
+
+# Heavy-tail index ---------------------------------------------------
+heavy_tail_ptw <- function(x, mu, phi, power) {
+    Px1 <- dptw(y = c(x, x+1), mu = mu, phi = phi, power = power, 
+               n_pts = 100, method = "laguerre")
+    LTI <- Px1[2]/Px1[1]
+    return(LTI)
+}
+heavy_tail_ptw <- Vectorize(heavy_tail_ptw, c("x"))
+
+# END-------------------------------------------------------------------
--- Script3.R
+++ Script3.R
@@ -6,27 +6,51 @@
 require(compoisson)
 
 # Computing mean and variance using numerical integration --------------
-moments_cp <- function(lambda, nu, order = 1) {
+moments_cp <- function(lambda, nu, order = 1, upper = 2*lambda) {
     aux_fc <- function(x, lambda, nu, order) {
         output <- (x^order)*dcom(x, lambda = lambda, nu = nu)
         return(output)
     }
-    output <- integrate(aux_fc, lower = 0, upper = lambda*2, 
+    output <- integrate(aux_fc, lower = 0, upper = upper, 
                         lambda = lambda, nu = nu, order = order)
-    return(output)
+    return(output$value)
 }
+moments_cp <- Vectorize(moments_cp, c("lambda"))
 
 # Approximated mean and variance ---------------------------------------
 ap_moments_cp <- function(lambda, nu) {
     expec <- lambda^(1/nu) - (nu - 1)/(2*nu)
     variance <- (1/nu)*(lambda^(1/nu))
-    return(c(expec, variance))
+    return(c("Expectation" = expec, "Variance" = variance))
 }
+ap_moments_cp <- Vectorize(ap_moments_cp, c("lambda"))
 
-di_cmp <- function(lambda, nu) {
-    tt <- ap_moments_cp(lambda = lambda, nu = nu)
-    return(tt[2]/tt[1])
+# Dispersion index ---------------------------------------------------
+disp_index_cp <- function(lambda, nu) {
+    TEMP <- ap_moments_cp(lambda = lambda, nu = nu)
+    DI <- TEMP[2]/TEMP[1]
+    return(DI)
 }
+disp_index_cp <- Vectorize(disp_index_cp, c("lambda"))
+
+# Zero-inflation index -----------------------------------------------
+zero_inflation_cp <- function(lambda, nu) {
+    PX0 <- dcom(x = 0, lambda = lambda, nu = nu)
+    mu <- ap_moments_cp(lambda = lambda, nu = nu)[1]
+    ZI <- 1 + log(PX0)/mu
+    return(ZI)
+}
+zero_inflation_cp <- Vectorize(zero_inflation_cp, c("lambda"))
+
+# Heavy-tail index ---------------------------------------------------
+heavy_tail_cp <- function(x, lambda, nu) {
+    Px1 <- dcom(x = c(x, x+1), lambda = lambda, nu = nu)
+    LTI <- Px1[2]/Px1[1]
+    return(LTI)
+}
+heavy_tail_cp <- Vectorize(heavy_tail_cp, c("x"))
+
+
 # Plot COM-Poisson mass function ---------------------------------------
 plot_cmp <- function(lambda, nu, title, grid_y = 0:100) {
     dens <- dcom(x = grid_y, lambda = lambda, nu = nu)
@@ -38,8 +62,9 @@ plot_cmp <- function(lambda, nu, title, grid_y = 0:100) {
 system_equation <- function(param, Expec, DI) {
     lambda <- param[1]
     nu <- param[2]
+    print(param)
     eq1 <- ap_moments_cp(lambda, nu)[1] -  Expec
-    eq2 <- di_cmp(lambda, nu) - DI
+    eq2 <- ap_moments_cp(lambda, nu)[2]/ap_moments_cp(lambda, nu)[1] - DI
     return(c(eq1, eq2))
 }
 
@@ -51,8 +76,8 @@ system_equation <- function(param, Expec, DI) {
 #multiroot(system_equation, Expec = 10, DI = 2, start = c(2, 0.5))
 #ap_moments_cp(lambda = 2.88, nu = 0.47)
 
-#multiroot(system_equation, Expec = 10, DI = 10, start = c(1, 0.01))
-#ap_moments_cp(lambda = 1.30, nu = 0.13)
+#multiroot(system_equation, Expec = 10, DI = 10, start = c(2.2, 1))
+#ap_moments_cp(lambda = 0.5, nu = 0.1)
 
 #multiroot(system_equation, Expec = 10, DI = 6, start = c(1.2, 0.13))
 
