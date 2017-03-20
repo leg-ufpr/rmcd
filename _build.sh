01bf5b452e08997a1383d409740ee53aae624fd8
Walmes Zeviani <walmes@ufpr.br>
2017-03-20 19:29:31 -0300

    Gets _build.sh from master branch improves CSS.

--- _build.sh
+++ _build.sh
@@ -1,1 +1,2 @@
-Rscript -e 'bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook")'
+#Rscript -e 'bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook")'
+Rscript -e 'bookdown::preview_chapter("Applications.Rmd", output_format = "bookdown::gitbook")'
